c2ffcc968c2bdca3671b45d6616238514f5a48d8
ghabrielferrari <ghcferrari@gmail.com>
2026-01-21 09:10:34 -0300

    feat: create project

---
+++ ImWithYou/ImWithYou/ImWithYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImWithYouApp.swift
+//  ImWithYou
+//
+//  Created by Gabriel Ferrari on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ImWithYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
